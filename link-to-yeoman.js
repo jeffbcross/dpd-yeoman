ce4895cab0f40bde474566cae84b4d70972d0124
Jeff Cross <middlefloor@gmail.com>
2013-02-09 16:42:07 -0700

    Fixed linking function to not be confused by local module.

--- link-to-yeoman.js
+++ link-to-yeoman.js
@@ -1,18 +1,18 @@
 var path = require('path')
-	,	fs = require('fs');
+	,	fs = require('fs')
+	, npm = require.resolve('npm')
+	,	generators;
 
 // In the absence of generator management
 // in yeoman, let's add ourselves to the
 // generators.
-console.info("No op for now.");
 
 //Find path to yeoman.
-var yeoman = require.resolve("yeoman");
-yeoman = path.resolve(yeoman, '../../');
-console.info('yeoman', yeoman);
-//Add symlink to the project root in yeoman
+yeoman = path.resolve(npm, '../../../', 'yeoman');
 
-var generators = path.resolve(yeoman, 'node_modules', 'yeoman-generators', 'lib', 'generators');
+//Get path to Yeoman generators
+generators = path.resolve(yeoman, 'node_modules', 'yeoman-generators', 'lib', 'generators');
+
 if (fs.existsSync(generators)) {
 	console.log("Applying symlink to ", generators);
 	fs.symlink(__dirname, path.resolve(generators, 'deployd'), 'dir', function (e) {
